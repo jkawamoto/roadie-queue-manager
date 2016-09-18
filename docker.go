977b09efab2a732ebbbb2e6181ec36928291a7d2
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-09-18 19:13:41 -0400

    Implement a docker cli based API wrapper.

--- docker.go
+++ docker.go
@@ -1,58 +1,93 @@
 package main
 
 import (
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/client"
-	"golang.org/x/net/context"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
 )
 
-type Docker struct {
-	client *client.Client
+// DockerRequester defines necessary methods to access to docker.
+type DockerRequester interface {
+	GetID(name string) (res string, err error)
+	DeleteContainer(id string) error
 }
 
-func NewDocker() (res Docker, err error) {
+// Docker defines an inteface of docker API.
+type Docker struct {
+}
 
-	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.24"}
-	client, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
+// NewDocker returns a new docker interface.
+func NewDocker() *Docker {
+	return &Docker{}
+}
+
+// GetID returns the ID associated with a given container name.
+func (docker *Docker) GetID(name string) (res string, err error) {
+
+	cmd := exec.Command("docker", "ps", "-aq", "-f", fmt.Sprintf("name=%s", name))
+	output, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+	defer output.Close()
+
+	err = cmd.Start()
 	if err != nil {
 		return
 	}
 
-	res = Docker{
-		client: client,
+	buf, err := ioutil.ReadAll(output)
+	if err != nil {
+		return
 	}
+	res = strings.TrimRight(string(buf), "\n")
+
+	err = cmd.Wait()
 	return
 
 }
 
-func (d Docker) DeleteContainer(ctx context.Context, name string) error {
+// CreateContainer creates a container based on a given image. The container will
+// have a given name and receive a given script.
+// This function will block when the container ends.
+func (docker *Docker) CreateContainer(image, name, script string) (err error) {
 
-	filter := filters.NewArgs()
-	filter.Add("name", name)
-
-	res, err := d.client.ContainerList(ctx, types.ContainerListOptions{
-		All:    true,
-		Quiet:  true,
-		Filter: filter,
-	})
+	// docker run -i --name {{.Name}} {{.Image}} < run.yml
+	cmd := exec.Command("docker", "run", "-i", "--name", name, image, "--no-shutdown")
+	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		return err
+		return
+	}
+	writer := bufio.NewWriter(stdin)
+
+	err = cmd.Start()
+	if err != nil {
+		return
 	}
 
-	for _, container := range res {
-
-		err := d.client.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{
-			RemoveVolumes: true,
-			RemoveLinks:   true,
-			Force:         true,
-		})
-		if err != nil {
-			return err
-		}
-
+	_, err = writer.WriteString(script)
+	if err != nil {
+		return
+	}
+	err = writer.Flush()
+	if err != nil {
+		return
+	}
+	err = stdin.Close()
+	if err != nil {
+		return
 	}
 
-	return nil
+	return cmd.Wait()
+
+}
+
+// DeleteContainer deletes a container associated with a given ID or name.
+func (docker *Docker) DeleteContainer(id string) error {
+
+	cmd := exec.Command("docker", "rm", "-f", id)
+	return cmd.Run()
 
 }
