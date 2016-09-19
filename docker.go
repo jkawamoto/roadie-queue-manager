6fbe05caf81763f3f329aacef76b9e0715de2f2d
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-09-19 17:40:53 -0400

    Add tests for docker.go.

--- docker.go
+++ docker.go
@@ -1,3 +1,24 @@
+//
+// docker.go
+//
+// Copyright (c) 2016 Junpei Kawamoto
+//
+// This file is part of Roadie queue manager.
+//
+// Roadie is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Roadie is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 package main
 
 import (
@@ -11,22 +32,33 @@ import (
 // DockerRequester defines necessary methods to access to docker.
 type DockerRequester interface {
 	GetID(name string) (res string, err error)
+	CreateContainer(image, name string, script []byte) (err error)
 	DeleteContainer(id string) error
 }
 
+// commandExecutor is a type of function takes command name and arguments,
+// and returns a pointer of exec.Cmd to be run a command.
+type commandExecutor func(string, ...string) *exec.Cmd
+
 // Docker defines an inteface of docker API.
 type Docker struct {
+	// Command executor which will be used to run any command in this class.
+	command commandExecutor
 }
 
 // NewDocker returns a new docker interface.
 func NewDocker() *Docker {
-	return &Docker{}
+	// Default command executor is `exec.Command`.
+	return &Docker{
+		command: exec.Command,
+	}
 }
 
 // GetID returns the ID associated with a given container name.
 func (docker *Docker) GetID(name string) (res string, err error) {
 
-	cmd := exec.Command("docker", "ps", "-aq", "-f", fmt.Sprintf("name=%s", name))
+	// docker ps -aq -f name=<name>
+	cmd := docker.command("docker", "ps", "-aq", "-f", fmt.Sprintf("name=%s", name))
 	output, err := cmd.StdoutPipe()
 	if err != nil {
 		return
@@ -52,10 +84,10 @@ func (docker *Docker) GetID(name string) (res string, err error) {
 // CreateContainer creates a container based on a given image. The container will
 // have a given name and receive a given script.
 // This function will block when the container ends.
-func (docker *Docker) CreateContainer(image, name, script string) (err error) {
+func (docker *Docker) CreateContainer(image, name string, script []byte) (err error) {
 
-	// docker run -i --name {{.Name}} {{.Image}} < run.yml
-	cmd := exec.Command("docker", "run", "-i", "--name", name, image, "--no-shutdown")
+	// docker run -i --name <name> <image> --no-shutdown
+	cmd := docker.command("docker", "run", "-i", "--name", name, image, "--no-shutdown")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		return
@@ -67,7 +99,7 @@ func (docker *Docker) CreateContainer(image, name, script string) (err error) {
 		return
 	}
 
-	_, err = writer.WriteString(script)
+	_, err = writer.Write(script)
 	if err != nil {
 		return
 	}
@@ -87,7 +119,8 @@ func (docker *Docker) CreateContainer(image, name, script string) (err error) {
 // DeleteContainer deletes a container associated with a given ID or name.
 func (docker *Docker) DeleteContainer(id string) error {
 
-	cmd := exec.Command("docker", "rm", "-f", id)
+	// docker run -f <id>
+	cmd := docker.command("docker", "rm", "-f", id)
 	return cmd.Run()
 
 }
---
+++ docker_test.go
@@ -0,0 +1,137 @@
+//
+// docker_test.go
+//
+// Copyright (c) 2016 Junpei Kawamoto
+//
+// This file is part of Roadie queue manager.
+//
+// Roadie is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Roadie is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+// Test to obtain a container ID.
+func TestGetID(t *testing.T) {
+
+	containerName := "sample-container-name"
+	containerID := "ABCDEFG"
+
+	var (
+		docker *Docker
+		res    string
+		err    error
+	)
+
+	// Test for existing container.
+	docker = &Docker{
+
+		command: func(name string, args ...string) *exec.Cmd {
+
+			cmd := name + " " + strings.Join(args, " ")
+			expected := fmt.Sprintf("docker ps -aq -f name=%s", containerName)
+			if cmd != expected {
+				t.Error("Generated command isn't correct:", cmd)
+			}
+
+			return exec.Command("echo", containerID)
+
+		},
+	}
+
+	res, err = docker.GetID(containerName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if res != containerID {
+		t.Error("GetID to an existing container returns a wrong container ID:", res)
+	}
+
+	// Test for non existing container.
+	docker = &Docker{
+		command: func(name string, args ...string) *exec.Cmd {
+			return exec.Command("echo")
+		},
+	}
+
+	res, err = docker.GetID(containerName)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if len(res) != 0 {
+		t.Error("GetID to non existing container returns a wrong container ID:", res)
+	}
+
+}
+
+// Test to create a container.
+func TestCreateContainer(t *testing.T) {
+
+	image := "jkawamoto/roadie-gcp"
+	containerName := "sample-container"
+	script := `sample
+  script
+  `
+
+	docker := &Docker{
+
+		command: func(name string, args ...string) *exec.Cmd {
+
+			cmd := name + " " + strings.Join(args, " ")
+			expected := fmt.Sprintf("docker run -i --name %s %s --no-shutdown", containerName, image)
+			if cmd != expected {
+				t.Error("Generated command isn't correct:", cmd)
+			}
+
+			return exec.Command("echo", fmt.Sprintf("\"%s\"", string(script)))
+
+		},
+	}
+
+	if err := docker.CreateContainer(image, containerName, []byte(script)); err != nil {
+		t.Error("CreateContainer returns an error:", err.Error())
+	}
+
+}
+
+// Test to delete a container.
+func TestDeleteContainer(t *testing.T) {
+
+	containerID := "ABCDEFG"
+	docker := &Docker{
+
+		command: func(name string, args ...string) *exec.Cmd {
+
+			cmd := name + " " + strings.Join(args, " ")
+			expected := fmt.Sprintf("docker rm -f %s", containerID)
+			if cmd != expected {
+				t.Error("Generated command isn't correct:", cmd)
+			}
+
+			return exec.Command("echo", containerID)
+
+		},
+	}
+
+	if err := docker.DeleteContainer(containerID); err != nil {
+		t.Error("DeleteContainer returns an error:", err.Error())
+	}
+
+}
