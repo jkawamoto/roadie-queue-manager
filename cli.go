cef52b67514015c61f0ca697f86a31d48e213332
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-09-19 23:34:28 -0400

    Update license comments.

--- cli.go
+++ cli.go
@@ -5,7 +5,7 @@
 //
 // This file is part of Roadie queue manager.
 //
-// Roadie is free software: you can redistribute it and/or modify
+// Roadie Queue Manager  is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
